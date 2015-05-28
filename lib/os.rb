69d5ad8494aa4ec39f9380344cc28de29366a51b
Kenneth Leung <kenneth@leungs.us>
2015-05-28 16:28:16 -0700

    Removed instance variables this is a singleton class.

--- lib/os.rb
+++ lib/os.rb
@@ -1,27 +1,29 @@
 require "os/version"
 require 'rbconfig'
+require 'singleton'
 
 class OS
   class UnknownError < StandardError
+  end
+  # http://stackoverflow.com/questions/11784109/detecting-operating-systems-in-ruby/20579735#20579735
+  attr_writer :host_os
 
+  def self.host_os
+    RbConfig::CONFIG['host_os']
   end
 
-  # http://stackoverflow.com/questions/11784109/detecting-operating-systems-in-ruby/20579735#20579735
   def self.host
-    @host ||= begin
-      host_os = RbConfig::CONFIG['host_os']
-      case host_os
-      when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-        :windows
-      when /darwin|mac os/
-        :macosx
-      when /linux/
-        :linux
-      when /solaris|bsd/
-        :unix
-      else
-        raise OS::UnknownError, "unknown os: #{host_os.inspect}"
-      end
+    case host_os
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      :macosx
+    when /linux/
+      :linux
+    when /solaris|bsd/
+      :unix
+    else
+      raise OS::UnknownError, "unknown os: #{host_os.inspect}"
     end
   end
 
